bf7a958c584c8f6bb84afeb0f69c09fd49aff62f
sangam2ishra <sssangamkr.mishra@gmail.com>
2025-02-27 14:02:34 +0530

    Starter1 changes

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def hello_world(request):
     return HttpResponse("Hello, world! This is our interneers-lab Django server. Making changes0...")
 
+def hello_name(request):
+    name=request.GET.get("name", "World")
+    # return HttpResponse(f"Hello, {name}!")
+    return JsonResponse({"message":f"Hello, {name}!"})
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_world),
+    path('hello/', hello_name),
 ]
