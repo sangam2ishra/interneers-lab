4645f2b8158f60805e85458820ff38df74ae7d65
sangam2ishra <sangamkr.mishra@gmail.com>
2025-04-05 19:08:42 +0530

    added automatic test

--- backend/product/apps.py
+++ backend/product/apps.py
@@ -9,5 +9,7 @@ class ProductConfig(AppConfig):
     def ready(self):
         #only run seed script when starting the server
         if 'runserver' in sys.argv:
+            from scripts.run_tests import run_tests
+            run_tests()
             from product.seed_categories import seed_categories
             seed_categories()
