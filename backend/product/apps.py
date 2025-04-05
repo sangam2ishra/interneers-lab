779be3247e145809a8427ff8133cf3e5858d3f62
sangam2ishra <sangamkr.mishra@gmail.com>
2025-04-05 16:07:48 +0530

    updated apps.py

--- backend/product/apps.py
+++ backend/product/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+import sys
 
 
 class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product'
+
+    def ready(self):
+        #only run seed script when starting the server
+        if 'runserver' in sys.argv:
+            from product.seed_categories import seed_categories
+            seed_categories()
